ba9b1b09b31ebba8a047d0cc2314af2afdfb73c7
Java_DZ <asssher.uz@gmail.com>
2024-01-01 14:32:24 +0500

    Animal2

---
+++ src/main/java/org/example/Animal2.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Animal2 {
+    private String name;
+
+    public Animal2(String name) {
+        this.name = name;
+    }
+
+    public void move() {
+        System.out.println(name + " is moving.");
+    }
+}
+
+    public class FlyingAnimal extends Animal {
+        public FlyingAnimal(String name) {
+            super(name);
+        }
+
+        @Override
+        public void move() {
+            System.out.println(name + " is flying.");
+        }
+    }
+
+    public class SwimmingRunningAnimal extends Animal {
+        public SwimmingRunningAnimal(String name) {
+            super(name);
+        }
+
+        @Override
+        public void move() {
+            System.out.println(name + " is swimming and running.");
+        }
+    }
+
+
